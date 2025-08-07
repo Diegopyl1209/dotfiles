df151070a37f53ec67d334fcfeab21b2bef6f005
Diego Peña y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2025-08-06 23:22:59 -0400

    Remove .emacs.d/post-init.el Remove .emacs.d/pre-early-init.el Remove .emacs.d/pre-init.el

--- dot_emacs.d/pre-early-init.el
+++
@@ -1,7 +0,0 @@
-;;; -*- no-byte-compile: t; lexical-binding: t; -*-
-
-;;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
-(setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
-(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
-
-(setq package-enable-at-startup nil)
